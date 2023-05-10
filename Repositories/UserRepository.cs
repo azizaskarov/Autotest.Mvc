28bbd37d63cc688df5ff9924d4d24ab42db5544c
Azizbek Askarov <azizasqarov773@gmail.com>
2023-05-10 16:50:44 +0500

    Add project files.

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,129 @@
+﻿using Autotest.Mvc.Models;
+using Autotest.Mvc.Services;
+using Microsoft.Data.Sqlite;
+
+namespace Autotest.Mvc.Repositories;
+
+public class UserRepository
+{
+    private readonly SqliteConnection _connection;
+    private readonly TicketRepository _ticketRepository;
+
+    public UserRepository(TicketRepository ticketRepository)
+    {
+        _ticketRepository = ticketRepository;
+        _connection = new SqliteConnection("Data source=avtotest.db");
+        _connection.Open();
+
+        CreateUserTable();
+    }
+
+    private void CreateUserTable()
+    {
+        var command = _connection.CreateCommand();
+        command.CommandText = "CREATE TABLE IF NOT EXISTS users(id TEXT UNIQUE, username TEXT NOT NULL, password TEXT, name TEXT, photo_url TEXT, current_ticket_index INTEGER DEFAULT 0)";
+        command.ExecuteNonQuery();
+    }
+
+    public void AddUser(User user)
+    {
+        var command = _connection.CreateCommand();
+        command.CommandText = "INSERT INTO users(id, username, password, name, photo_url) VALUES(@id, @username, @password, @name, @photo_url)";
+        command.Parameters.AddWithValue("id", user.Id);
+        command.Parameters.AddWithValue("username", user.Username);
+        command.Parameters.AddWithValue("password", user.Password);
+        command.Parameters.AddWithValue("name", user.Name);
+        command.Parameters.AddWithValue("photo_url", user.PhotoPath);
+        command.Prepare();
+        command.ExecuteNonQuery();
+    }
+
+    public User? GetUserById(string id)
+    {
+        return GetUser("id", id);
+    }
+    public User? GetUserByUsername(string username)
+    {
+        return GetUser("username", username);
+    }
+
+    public User? GetUser(string paramName, string paramValue)
+    {
+        var command = _connection.CreateCommand();
+        command.CommandText = $"SELECT * FROM users WHERE {paramName} = @p";
+        command.Parameters.AddWithValue("p", paramValue);
+        command.Prepare();
+        var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            var user =  new User()
+            {
+                Id = (string)reader["id"],
+                Username = reader.GetString(1),
+                Password = reader.GetString(2),
+                Name = reader.GetString(3),
+                PhotoPath = reader.GetString(4),
+                CurrentTicketIndex = reader.GetInt32(5),
+            };
+
+            user.CurrentTicket = user.CurrentTicketIndex == null ? null 
+                : _ticketRepository.GetTicket(user.CurrentTicketIndex.Value);
+            user.Tickets = _ticketRepository.GetTicketList(user.Id);
+
+            reader.Close();
+
+            return user;
+        }
+
+        reader.Close();
+
+        return null;
+    }
+
+    public void UpdateUser(User user)
+    {
+        var command = _connection.CreateCommand();
+        command.CommandText = "UPDATE users SET username = @username, password = @password, name = @name, photo_url = @photo_url, current_ticket_index = @index WHERE id = @id";
+        command.Parameters.AddWithValue("id", user.Id);
+        command.Parameters.AddWithValue("username", user.Username);
+        command.Parameters.AddWithValue("password", user.Password);
+        command.Parameters.AddWithValue("name", user.Name);
+        command.Parameters.AddWithValue("photo_url", user.PhotoPath);
+        command.Parameters.AddWithValue("index", user.CurrentTicketIndex);
+        command.Prepare();
+        command.ExecuteNonQuery();
+    }
+
+    public List<User> GetUsers()
+    {
+        var command = _connection.CreateCommand();
+        command.CommandText = "SELECT * FROM users ORDER BY id DESC LIMIT 10";
+        var reader = command.ExecuteReader();
+
+        var users = new List<User>();
+
+        while (reader.Read())
+        {
+            var user = new User()
+            {
+                Id = (string)reader["id"],
+                Username = reader.GetString(1),
+                Password = reader.GetString(2),
+                Name = reader.GetString(3),
+                PhotoPath = reader.GetString(4),
+                CurrentTicketIndex = reader.GetInt32(5),
+            };
+
+            user.CurrentTicket = user.CurrentTicketIndex == null ? null
+                : _ticketRepository.GetTicket(user.CurrentTicketIndex.Value);
+            user.Tickets = _ticketRepository.GetTicketList(user.Id);
+
+            users.Add(user);
+        }
+
+        reader.Close();
+        return users;
+    }
+
+}
---
+++ Services/UsersService.cs
@@ -0,0 +1,116 @@
+﻿using System.Net.NetworkInformation;
+using Autotest.Mvc.Models;
+using Autotest.Mvc.Repositories;
+
+namespace Autotest.Mvc.Services;
+
+public class UsersService
+{
+    private const string UserIdCookieKey = "user_id";
+    public readonly TicketRepository TicketRepository;
+    public readonly UserRepository UserRepository;
+    private readonly QuestionService _questionService;
+
+    public UsersService(TicketRepository ticketRepository, UserRepository userRepository, QuestionService questionService)
+    {
+        TicketRepository = ticketRepository;
+        UserRepository = userRepository;
+        _questionService = questionService;
+    }
+
+    public void Register(CreateUserModel createUser, HttpContext httpContext)
+    {
+        var user = new User
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = createUser.Name!,
+            Password = createUser.Password!,
+            Username = createUser.Username!,
+            PhotoPath = SavePhoto(createUser.Photo!)
+        };
+
+        user.CurrentTicket = user.CurrentTicketIndex == null ? null
+            : TicketRepository.GetTicket(user.CurrentTicketIndex.Value);
+        user.Tickets = TicketRepository.GetTicketList(user.Id);
+
+        CreateUserTickets(user);
+
+        UserRepository.AddUser(user);
+
+        httpContext.Response.Cookies.Append(UserIdCookieKey, user.Id);
+    }
+
+    public bool LogIn(SignInUserModel signInUserModel, HttpContext httpContext)
+    {
+        var user = UserRepository.GetUserByUsername(signInUserModel.Username);
+
+        if (user == null || user.Password != signInUserModel.Password)
+            return false;
+
+        //v2
+        if (user?.Password != signInUserModel.Password)
+            return false;
+
+        httpContext.Response.Cookies.Append(UserIdCookieKey, user.Id);
+
+        return true;
+    }
+
+    public User? GetCurrentUser(HttpContext context)
+    {
+        if (context.Request.Cookies.ContainsKey(UserIdCookieKey))
+        {
+            var userId = context.Request.Cookies[UserIdCookieKey];
+            var user = UserRepository.GetUserById(userId);
+
+            return user;
+        }
+
+        return null;
+    }
+
+    public bool IsLoggedIn(HttpContext context)
+    {
+        if (!context.Request.Cookies.ContainsKey(UserIdCookieKey)) return false;
+
+        var userId = context.Request.Cookies[UserIdCookieKey];
+        var user = UserRepository.GetUserById(userId);
+
+        return user != null;
+    }
+
+    public void LogOut(HttpContext httpContext)
+    {
+        httpContext.Response.Cookies.Delete(UserIdCookieKey);
+    }
+
+    private void CreateUserTickets(User user)
+    {
+        for (var i = 0; i < _questionService.TicketsCount; i++)
+        {
+            var startIndex = i * _questionService.TicketQuestionsCount + 1;
+            
+            TicketRepository.AddTicket(new Ticket()
+            {
+                Id = i,
+                UserId = user.Id,
+                CurrentQuestionIndex = startIndex,
+                StartIndex = startIndex,
+                QuestionsCount = _questionService.TicketQuestionsCount
+            });
+        }
+    }
+
+    private string SavePhoto(IFormFile file)
+    {
+        if (!Directory.Exists("wwwroot/UserImages"))
+            Directory.CreateDirectory("wwwroot/UserImages");
+
+        var fileName = Guid.NewGuid() + ".jpg";
+        var ms = new MemoryStream();
+        file.CopyTo(ms);
+        System.IO.File.WriteAllBytes(Path.Combine("wwwroot", "UserImages", fileName), ms.ToArray());
+
+        return "/UserImages/" + fileName;
+    }
+}
